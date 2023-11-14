baab9c8939098d62f1881d1172175ca60348934d
PHANTOMGOD <phantom.god.dev@gmail.com>
2023-11-14 12:30:29 +0530

    Update pinecone.ts

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -5,7 +5,7 @@ export const getPineconeClient = async () => {
 
   await client.init({
     apiKey: process.env.PINECONE_API_KEY!,
-    environment: "gcp-starter",
+    environment: "gcp-starter-free",
   })
 
   return client
