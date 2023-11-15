73f07480b290eb23575e40e6ba1133f631592385
PHANTOMGOD <phantom.god.dev@gmail.com>
2023-11-15 22:42:16 +0530

    Update pinecone.ts

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -2,7 +2,7 @@ import { Pinecone } from "@pinecone-database/pinecone";
 
 
 export const getPinecone = async () => {
-  const client = new Pinecone():
+  const client = new Pinecone();
 
   await client.init({
     apiKey: process.env.PINECONE_API_KEY!,
