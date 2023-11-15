5c5f0b430470032f526682f36d29b8b5961c63e8
PHANTOMGOD <phantom.god.dev@gmail.com>
2023-11-16 03:23:33 +0530

    Update route.ts

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,34 +1,36 @@
-import { db } from "@/db";
-import { openai } from "@/lib/openai";
-import { pinecone } from "@/lib/pinecone";
-import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { PineconeStore } from "langchain/vectorstores/pinecone";
-import { NextRequest } from "next/server";
+import { db } from '@/db'
+import { openai } from '@/libs/openai'
+import { pinecone } from '@/libs/pinecone'
+import { SendMessageValidator } from '@/libs/validators/SendMessageValidator'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { PineconeStore } from 'langchain/vectorstores/pinecone'
+import { NextRequest } from 'next/server'
 
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import { OpenAIStream, StreamingTextResponse } from 'ai'
 
-// endpoint for asking a question to a pdf file
 export const POST = async (req: NextRequest) => {
-  const body = await req.json();
+  // endpoint for asking a question to a pdf file
 
-  const { getUser } = getKindeServerSession();
-  const user = getUser();
+  const body = await req.json()
 
-  const { id: userId } = user;
-  if (!userId) return new Response("Unauthorized", { status: 401 });
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
 
-  const { fileId, message } = SendMessageValidator.parse(body);
+  const { id: userId } = user
+
+  if (!userId) return new Response('Unauthorized', { status: 401 })
+
+  const { fileId, message } = SendMessageValidator.parse(body)
 
   const file = await db.file.findFirst({
     where: {
       id: fileId,
       userId,
     },
-  });
+  })
 
-  if (!file) return new Response("Not found", { status: 404 });
+  if (!file) return new Response('Not found', { status: 404 })
 
   await db.message.create({
     data: {
@@ -37,70 +39,68 @@ export const POST = async (req: NextRequest) => {
       userId,
       fileId,
     },
-  });
+  })
 
   // 1: vectorize message
   const embeddings = new OpenAIEmbeddings({
     openAIApiKey: process.env.OPENAI_API_KEY,
-  });
+  })
 
-  const pineconeIndex = pinecone.Index("quill").namespace(userId);
+  const pineconeIndex = pinecone.Index('quill')
 
   const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
     pineconeIndex,
-  });
+    // namespace: file.id,
+  })
 
-  // TODO: paid users could get higher top K
-  const results = await vectorStore.similaritySearch(message, 4, {
-    "file.id": fileId,
-  });
+  const results = await vectorStore.similaritySearch(message, 4)
 
   const prevMessages = await db.message.findMany({
     where: {
       fileId,
     },
     orderBy: {
-      createdAt: "asc",
+      createdAt: 'asc',
     },
     take: 6,
-  });
+  })
 
   const formattedPrevMessages = prevMessages.map((msg) => ({
-    role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
+    role: msg.isUserMessage ? ('user' as const) : ('assistant' as const),
     content: msg.text,
-  }));
+  }))
 
   const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
+    model: 'gpt-3.5-turbo',
     temperature: 0,
     stream: true,
     messages: [
       {
-        role: "system",
+        role: 'system',
         content:
-          "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+          'Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.',
       },
       {
-        role: "user",
+        role: 'user',
         content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
-
+        
   \n----------------\n
-
+  
   PREVIOUS CONVERSATION:
   ${formattedPrevMessages.map((message) => {
-    if (message.role === "user") return `User: ${message.content}\n`;
-    return `Assistant: ${message.content}\n`;
+    if (message.role === 'user') return `User: ${message.content}\n`
+    return `Assistant: ${message.content}\n`
   })}
-
+  
   \n----------------\n
-
+  
   CONTEXT:
-  ${results.map((r) => r.pageContent).join("\n\n")}
-
+  ${results.map((r) => r.pageContent).join('\n\n')}
+  
   USER INPUT: ${message}`,
       },
     ],
-  });
+  })
 
   const stream = OpenAIStream(response, {
     async onCompletion(completion) {
@@ -111,9 +111,9 @@ export const POST = async (req: NextRequest) => {
           fileId,
           userId,
         },
-      });
+      })
     },
-  });
+  })
 
-  return new StreamingTextResponse(stream);
-};
+  return new StreamingTextResponse(stream)
+}
