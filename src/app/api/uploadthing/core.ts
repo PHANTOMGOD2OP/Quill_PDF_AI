4bcf389801e6fdd4adced1d03951701e0dd01e95
PHANTOMGOD <phantom.god.dev@gmail.com>
2023-11-16 03:25:05 +0530

    Update core.ts

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,132 +1,127 @@
-import { PLANS } from "@/config/stripe";
-import { db } from "@/db";
-import { pinecone } from "@/lib/pinecone";
-import { getUserSubscriptionPlan } from "@/lib/stripe";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { PineconeStore } from "langchain/vectorstores/pinecone";
-import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { db } from '@/db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { createUploadthing, type FileRouter } from 'uploadthing/next'
 
-const f = createUploadthing();
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { PineconeStore } from 'langchain/vectorstores/pinecone'
+import { pinecone } from '@/libs/pinecone'
+import { getUserSubscriptionPlan } from '@/libs/stripe'
+import { PLANS } from '@/config/stripe'
+
+const f = createUploadthing()
 
 const middleware = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = getUser();
-  if (!user || !user.id) {
-    throw new Error("Unauthorized");
-  }
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
 
-  const subscriptionPlan = await getUserSubscriptionPlan();
+  if (!user || !user.id) throw new Error('Unauthorized')
 
-  return { subscriptionPlan, userId: user.id };
-};
+  const subscriptionPlan = await getUserSubscriptionPlan()
+
+  return { subscriptionPlan, userId: user.id }
+}
 
 const onUploadComplete = async ({
   metadata,
   file,
 }: {
-  metadata: Awaited<ReturnType<typeof middleware>>;
+  metadata: Awaited<ReturnType<typeof middleware>>
   file: {
-    key: string;
-    name: string;
-    url: string;
-  };
+    key: string
+    name: string
+    url: string
+  }
 }) => {
-  const isFileExists = await db.file.findFirst({
+  const isFileExist = await db.file.findFirst({
     where: {
       key: file.key,
     },
-  });
+  })
 
-  if (isFileExists) return;
+  if (isFileExist) return
 
   const createdFile = await db.file.create({
     data: {
       key: file.key,
+      name: file.name,
       userId: metadata.userId,
       url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-      name: file.name,
-      uploadStatus: "PROCESSING",
+      uploadStatus: 'PROCESSING',
     },
-  });
+  })
 
   try {
     const response = await fetch(
       `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
-    );
-    const blob = await response.blob();
-    const loader = new PDFLoader(blob);
-    const pageLevelDocs = (await loader.load()).map((doc) => {
-      return {
-        ...doc,
-        metadata: {
-          ...doc.metadata,
-          "file.id": createdFile.id,
-        },
-      };
-    });
+    )
 
-    const pagesAmt = pageLevelDocs.length;
-    const { subscriptionPlan } = metadata;
-    const { isSubscribed } = subscriptionPlan;
+    const blob = await response.blob()
+
+    const loader = new PDFLoader(blob)
+
+    const pageLevelDocs = await loader.load()
+
+    const pagesAmt = pageLevelDocs.length
+
+    const { subscriptionPlan } = metadata
+    const { isSubscribed } = subscriptionPlan
 
     const isProExceeded =
-      pagesAmt > PLANS.find((plan) => plan.name === "Pro")!.pagesPerPdf;
+      pagesAmt > PLANS.find((plan) => plan.name === 'Pro')!.pagesPerPdf
     const isFreeExceeded =
-      pagesAmt > PLANS.find((plan) => plan.name === "Free")!.pagesPerPdf;
+      pagesAmt > PLANS.find((plan) => plan.name === 'Free')!.pagesPerPdf
 
     if ((isSubscribed && isProExceeded) || (!isSubscribed && isFreeExceeded)) {
       await db.file.update({
         data: {
-          uploadStatus: "FAILED",
+          uploadStatus: 'FAILED',
         },
         where: {
           id: createdFile.id,
         },
-      });
+      })
     }
 
-    const pineconeIndex = await pinecone
-      .Index("quill")
-      .namespace(metadata.userId);
+    // vectorize and index entire document
+    const pineconeIndex = pinecone.Index('quill')
 
     const embeddings = new OpenAIEmbeddings({
       openAIApiKey: process.env.OPENAI_API_KEY,
-    });
+    })
 
     await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
       pineconeIndex,
-    });
+      // namespace: createdFile.id,
+    })
 
     await db.file.update({
       data: {
-        uploadStatus: "SUCCESS",
+        uploadStatus: 'SUCCESS',
       },
       where: {
         id: createdFile.id,
       },
-    });
-  } catch (error) {
-    console.log("error: ", error);
+    })
+  } catch (err) {
     await db.file.update({
       data: {
-        uploadStatus: "FAILED",
+        uploadStatus: 'FAILED',
       },
       where: {
         id: createdFile.id,
       },
-    });
+    })
   }
-};
+}
 
 export const ourFileRouter = {
-  freePlanUploader: f({ pdf: { maxFileSize: "4MB" } })
+  freePlanUploader: f({ pdf: { maxFileSize: '4MB' } })
     .middleware(middleware)
     .onUploadComplete(onUploadComplete),
-  proPlanUploader: f({ pdf: { maxFileSize: "16MB" } })
+  proPlanUploader: f({ pdf: { maxFileSize: '16MB' } })
     .middleware(middleware)
     .onUploadComplete(onUploadComplete),
-} satisfies FileRouter;
+} satisfies FileRouter
 
-export type OurFileRouter = typeof ourFileRouter;
+export type OurFileRouter = typeof ourFileRouter
